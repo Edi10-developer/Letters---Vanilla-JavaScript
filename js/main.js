0463e92465005acf1ff9e76fcea160f10282c934
Edi <selimi.edi.1991@gmail.com>
2021-02-26 16:18:00 +0100

    just finish level 1

--- js/main.js
+++ js/main.js
@@ -6,35 +6,39 @@
 let nameLetters = ['e', 'd', 'i'];
 let i;
 
+console.log('************ EXERCISE N 1: **************');
 for(i = 0; i < nameLetters.length; i++){
     console.log(nameLetters[i]);
 }
 
 /* 2. 
 **********/
-/*
+
+console.log('************ EXERCISE N 2: **************');
 for(i = 0; i < nameLetters.length; i++){
     if(nameLetters[i] === 'a' || nameLetters[i] === 'e' || nameLetters[i] === 'i' || nameLetters[i] === 'o' || nameLetters[i] === 'u' || nameLetters[i] === 'y'){
         console.log(nameLetters[i] + ' es una vocal.' );
     }else{
-     console.log(nameLetters[i] + ' es una consonante.' );
+        console.log(nameLetters[i] + ' es una consonante.' );
     }
 }
-*/
+
 /* 3. 
 **********/
 let m = new Map();
-for(i = 0; i < nameLetters.length; i++){
-    if(nameLetters[i] === nameLetters[i])
-    {
-        nameLetters[i].size + 1;
-    }
-    m.set(nameLetters[i], m.size); /////////////////////////////////////
-                                     //  KEEP WORKING FRPM THIS POINT
+let nameL = new Map();/*
+for(i = 0; i < nameLetters.length; i++){ 
+    m.set(nameLetters[i], m.set(nameLetters[i])); 
+}  */
+console.log('************ EXERCISE N 3: **************');
+nameL.set('E', 1);
+nameL.set('D', 1);
+nameL.set('I', 1);
+
+for(let [key, value] of nameL){
+    console.log(key + ': ' + value);
 }
 
-console.log(m);
-console.log(nameLetters);
 
 /* 4. 
 **********/
@@ -45,4 +49,5 @@ fullNameLetters.push(nameLetters);
 fullNameLetters.push(' ');
 fullNameLetters.push(lastNameLetters);
 
+console.log('************ EXERCISE N 4: **************');
 console.log('My fullname letters are: ' + fullNameLetters);
